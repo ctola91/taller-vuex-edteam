f099a8d99b70aeba863141f48944feb39487b73b
ctola91 <ctolapacheco@gmail.com>
2021-10-20 00:36:21 -0400

    add mutations

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,19 +1,25 @@
 <template>
   <h1>Categorias</h1>
-
+  <input type="text" v-model="text" />
+  <button @click="setMessage">Cambiar</button>
   <p v-show="error">{{ message }}</p>
 </template>
 <script>
-import { computed } from "vue";
+import { ref, computed } from "vue";
 import { useStore } from "vuex";
 
 export default {
   setup() {
+    const text = ref("");
     const store = useStore();
 
     return {
+      text,
       message: computed(() => store.state.message),
       error: computed(() => store.state.error),
+      setMessage: () => {
+        store.commit("setMessage", text.value);
+      },
     };
   },
 };

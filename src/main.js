02f32d747e415845a0fe7dae8750f8a57a3051b3
ctola91 <ctolapacheco@gmail.com>
2021-10-28 00:06:10 -0400

    Add Chart component

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from "./store";
 import router from "./router";
 
 import "bulma/css/bulma.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const app = createApp(App);
 app.use(router);
